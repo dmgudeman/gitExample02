982b8887bb3e4d7d57d1438cee570124cfde9655
David M Gudeman <davidmgudeman@gmail.com>
2015-06-02 15:27:24 -0700

    first commit

---
+++ src/gitExample02/Main.java
@@ -0,0 +1,14 @@
+package gitExample02;
+
+public class Main
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hi there");
+      
+      System.out.println("kludge");
+
+   }
+
+}
